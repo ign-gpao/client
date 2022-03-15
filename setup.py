4b3a6d41ce7d9cf1bc5c05730675d536e566be31
Arnaud Birk <arnaud.birk@ign.fr>
2022-03-15 14:37:13 +0100

    feat(deploy) add setup.py for building package

---
+++ setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="ign-gpao-client",
+    version="0.1.0",
+    description="Client GPAO",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url="https://github.com/ign-gpao/client-gpao.git",
+    author="Arnaud Birk",
+    author_email="arnaud.birk@ign.fr",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["client"],
+    include_package_data=True,
+)
