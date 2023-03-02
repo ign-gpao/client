7b550b7b01e4073e0c08476fe2b66bbad1439583
Guillaume Liégard <guillaume.liegard@gmail.com>
2023-03-02 22:16:02 +0100

    test: test/loop4 is a test that print few lines, then wait. Now the client doesn't append logs

--- test/test_print_loop.py
+++ test/test_print_loop.py
@@ -26,8 +26,9 @@ def test_1_create_gpao():
     job = Job("test_loop_should_last_lest_than_1_sec", "python -u ../test/loop.py")
     job2 = Job("test_loop2_should_last_30_sec", "python -u ../test/loop2.py")
     job3 = Job("test_loop3_should_last_30_sec", "python -u ../test/loop3.py")
+    job4 = Job("test_loop4_should_print_lines_last_30_sec", "python -u ../test/loop4.py")
 
-    project1 = Project("test_client_print_loop", [job, job2, job3])
+    project1 = Project("test_client_print_loop", [job, job2, job3, job4])
  
     builder = Builder([project1])
     builder.save_as_json(FILENAME)
