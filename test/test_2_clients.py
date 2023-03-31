303a5288baa9dae47278c70b3a6ecb95d11e4cec
Guillaume Liégard <guillaume.liegard@gmail.com>
2023-03-31 13:53:01 +0200

    test: new test to launch 2 clients

---
+++ test/test_2_clients.py
@@ -0,0 +1,35 @@
+from client import worker
+
+import time
+import pytest
+
+from client_proc import ClientProc
+
+
+def test_client_normal_start_stop():
+    alice = ClientProc("Alice").start("-c")
+    alice.stop().assert_exit(0)
+
+
+def test_client_with_existing_session_exit1():
+    ClientProc("Alice").start("-c").stop_hard()
+    ClientProc("Alice").start().assert_exit(1)
+
+
+def test_client_with_existing_session_start_with_option_clean():
+    alice = ClientProc("Alice").start("-c").stop_hard()
+    
+    alice = ClientProc("Alice").start("-c").assert_is_running()
+    alice.stop().assert_exit(0)
+
+
+@pytest.mark.xfail(reason="not implemented yet")
+def test_2_client_cleaning():
+    alice = ClientProc("Alice").start()    
+    bob = ClientProc("Bob").start("--clean")
+
+    # issue with the folder comes when client scan disk space in the specific folder
+    time.sleep(30)
+
+    alice.stop().assert_exit()
+    bob.stop().assert_exit()
